4a994216d04bae3812141e33ba3b28453db8f24e
daniel cruz <dannyfsociety@gmail.com>
2018-12-09 12:29:31 -0400

    Reactive Forms

--- src/app/components/form-data/form-data.component.ts
+++ src/app/components/form-data/form-data.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-data',
   templateUrl: './form-data.component.html',
   styleUrls: ['./form-data.component.css']
 })
-export class FormDataComponent implements OnInit {
+export class FormDataComponent {
+  myForm: FormGroup;  
+  constructor() { 
+    this.myForm = new FormGroup({
+      'name': new FormControl('', [Validators.required, Validators.minLength(5)] ),
+      'lastname': new FormControl('', [Validators.required, Validators.minLength(5)] ),
+      'email': new FormControl('',  [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')])
+    });
+  }
 
-  constructor() { }
-
-  ngOnInit() {
+  saveData() {
+    console.log(this.myForm);
+    console.log(this.myForm.value);
   }
 
 }
